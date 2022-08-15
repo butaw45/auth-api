e002c3df62fa0f3e0b63e120e5dba6643836aa58
haris <kumantheas@gmail.com>
2022-08-15 09:21:46 +0700

    endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
-import express from "express";
+import express from 'express';
+import config from './app/config';
+import authRouter from './app/auth/router';
 
 const app = express();
-const port = 3000;
+const port = config.port || 3000;
+
+app.use(express.json());
+
+app.use('/api/v1', authRouter);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
