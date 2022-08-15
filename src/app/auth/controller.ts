3d313f9c1420cba22eb1e646810a55d027163bfa
haris <kumantheas@gmail.com>
2022-08-15 13:34:20 +0700

    login

--- src/app/auth/controller.ts
+++ src/app/auth/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import db from '../../data/users.json';
 import argon from 'argon2';
-import fs from 'fs/promises';
-import path from 'path';
-
+import jwt from 'jsonwebtoken';
+import config from '../config';
+import { createFile } from '../../utils/createFile';
 interface payloadType {
   email: string;
   password: string;
@@ -25,12 +25,49 @@ class Auth {
       const newUser = { ...payload, password: encrypt };
       const setUser = [...db, newUser];
 
-      await fs.writeFile(
-        path.join(__dirname, '..', '..', 'data', 'users.json'),
-        JSON.stringify(setUser)
-      );
+      await createFile(setUser);
 
       return res.status(200).json({ message: 'register success' });
+    } catch (error) {
+      return next(error);
+    }
+  }
+  async login(req: Request, res: Response, next: NextFunction) {
+    const payload = req.body as payloadType;
+
+    if (payload.email.length < 1 || payload.password.length < 1) {
+      return res.status(400).json({ message: 'email or password required' });
+    }
+
+    const user = db.filter((item) => item.email === payload.email);
+    if (user.length < 1)
+      return res.status(404).json({ message: 'email or password wrong' });
+
+    try {
+      const pwdMatch = await argon.verify(user[0].password, payload.password);
+      if (!pwdMatch)
+        return res.status(404).json({ message: 'email or password wrong' });
+
+      const accessToken = jwt.sign(
+        { email: payload.email },
+        config.accessKey as string,
+        { expiresIn: '30s' }
+      );
+      const refreshToken = jwt.sign(
+        { email: payload.email },
+        config.refreshKey as string,
+        { expiresIn: '1d' }
+      );
+
+      const otherUser = db.filter((item) => item.email !== payload.email);
+      const currentUser = { ...user[0], refreshToken };
+      const setUser = [...otherUser, currentUser];
+
+      const oneDay = 24 * 60 * 60 * 1000;
+
+      await createFile(setUser);
+      res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: oneDay });
+      return res.status(200).json({ token: accessToken });
     } catch (error) {
       return next(error);
     }
--- src/app/auth/router.ts
+++ src/app/auth/router.ts
@@ -5,5 +5,7 @@ const router = Router();
 const authController = new controller();
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
+// router.post('/login', authController.login);
 
 export default router;
