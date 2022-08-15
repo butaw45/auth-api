c119c0d512024a5de44172c2303a3c03af351829
haris <kumantheas@gmail.com>
2022-08-15 16:32:10 +0700

    verify auth token

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from './app/config';
 import authRouter from './app/auth/router';
+import commentRouter from './app/comment/router';
 
 const app = express();
 const port = config.port || 3000;
@@ -8,5 +9,6 @@ const port = config.port || 3000;
 app.use(express.json());
 
 app.use('/api/v1', authRouter);
+app.use('/api/v1', commentRouter);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
